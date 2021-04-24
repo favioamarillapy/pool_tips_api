5242b796dce3d2ca01af247aa2bd03aba3a3be84
Matias Báez <matiasbaez@users.noreply.github.com>
2021-04-24 16:56:53 +0100

    Added, tips controller and routes

---
+++ app/Http/Controllers/TipsController.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tips;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\BaseController as BaseController;
+
+class TipsController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        if ($request->expectsJson()) {
+
+            $query = Tips::select('*');
+
+            $list = $request->get('list');
+            $listBoolean = filter_var($list, FILTER_VALIDATE_BOOLEAN);
+    
+            $action = $listBoolean ? 'get' : 'paginate';
+            $tips = $query->$action();
+
+            return $this->sendResponse(true, 'Listado obtenido exitosamente', $tips, 200);
+
+        }
+
+        return $this->sendResponse(false, 'Metodo no permitido', null, 400);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $name = $request->get('name');
+        $description = $request->get('description');
+        $active = $request->get('active');
+
+        $tip = new Tips();
+        $tip->name = $name;
+        $tip->description = $description;
+        $tip->active = $active ? $active : 1;
+
+        if ($tip->save()) {
+
+            if ($request->hasFile('image')) {
+            
+            }
+
+            return $this->sendResponse(true, 'El consejo ha sido registrado correctamente', $tip, 201);
+        }
+
+        return $this->sendResponse(false, 'Ha ocurrido un problema al intentar registrar el consejo', null, 500);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $name = $request->get('name');
+        $description = $request->get('description');
+        $active = $request->get('active');
+
+        $tip = Tips::find($id);
+        $tip->name = $name;
+        $tip->description = $description;
+        $tip->active = $active ? $active : 1;
+
+        if ($tip->save()) {
+
+            if ($request->hasFile('image')) {
+            
+            }
+
+            return $this->sendResponse(true, 'El consejo ha sido actualizado correctamente', $tip, 200);
+        }
+
+        return $this->sendResponse(false, 'Ha ocurrido un problema al intentar actualizar el consejo', null, 500);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TipsController;
 use App\Http\Controllers\UserController;
 
 /*
@@ -30,3 +31,5 @@ Route::group(['prefix' => 'auth'], function () {
         Route::post('logout', [AuthController::class, 'logout']);    
     });
 });
+
+Route::resource('tips', TipsController::class);
